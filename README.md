b85498f348fcf7edcbc712185deb912fb11c1e7d
mrchimp <chimpytk@gmail.com>
2014-10-01 15:24:34 +0100

    First commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+
+HTML Like this:
+
+    <link rel="stylesheet" type="text/css" href="insetimg.jquery.css">
+
+    <script type="text/javascript" src="jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="insetimg.jquery.js"></script>
+
+    <div class="insetimg-frame">
+      <img src="foo.jpg">
+    </div>
+    <div class="insetimg-frame">
+      <img src="bar.jpg">
+    </div>
+
+
+Javascript Like this:
+
+    $('.insetimg-frame').insetimg();
+
---
+++ insetimg.jquery.js
@@ -0,0 +1,69 @@
+(function ($) {
+
+  $.fn.insetimg = function (options) {
+
+    var elems = this,
+        images = [],
+        wrapper,
+        wrap_inner,
+        x;
+
+    function doResize () {
+      $('.insetimg-image').height($(window).height());
+      doScroll();
+    }
+
+    function doScroll () {
+      var x = 0,
+          scrollTop = $(window).scrollTop();
+
+      elems.each(function() {
+        var offset = $(this).offset().top,
+            top_dist = (offset - scrollTop) + $(this).height(),
+            top_perc = 100 - (top_dist / $(window).height()) * 100;
+
+        $('.insetimg-wrapper:eq('+x+')').css('bottom', top_perc + '%');
+
+        x++;
+      });
+    }
+
+    wrapper = $('<div />').
+        addClass('insetimg');
+
+    wrap_inner = $('<div />').
+      addClass('insetimg-inner')
+      .appendTo(wrapper);
+    
+    x = 0;
+
+    $(this).each(function () {
+      var inner = $('<div />')
+        .addClass('insetimg-image')
+        .css({
+          'background-image': 'url(' + $(this).find('img').attr('src') +')'
+        });
+
+      $('<div />')
+        .addClass('insetimg-wrapper')
+        .css('z-index', elems.length - x + 1)
+        .html(inner)
+        .appendTo(wrap_inner);
+
+      $(this).addClass('loaded').find('img').remove();
+
+      x++;
+    });
+
+    wrapper.prependTo($('body'));
+
+    $(window).on('scroll', doScroll).on('resize', doResize);
+    
+    doResize();
+    doScroll();
+
+    return this;
+
+  };
+
+}(jQuery));
